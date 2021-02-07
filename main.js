92816dd01c8db2e8f947177534414f45e8d2b6dd
ayushidubal <64092360+ayushidubal@users.noreply.github.com>
2021-02-07 21:53:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,411 @@
+require.config({
+    waitSeconds: 60,
+    paths: {
+        'sdk': uiBuilderSDKPath + '/sdk',
+    }
+});
+
+define(['sdk'], BackendlessUI => BackendlessUI.startApp());
+
+define('./pages/404/components/902c9641de4ec9e73427e7192a118d14/bundle.js', [], () => ({
+  /* content */
+
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick *//* content */
+}));
+
+define('./pages/404/components/9ea586cd3d73a464ea46f38f4e5a5b6c/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('AllTutorials', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AboutUs/components/cc14d641a62224719e9ee65fefcdd4cb/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/c1a3605ba0a5aa26904ffca42c4d7e07/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('ChangeTyre', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/25a537e4845feb5c78d4082b92ca7231/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('ChangeBulb', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/c4747835036da08d4547993d38ab24c3/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('SelfDefense', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/a21b2b6d1bcdf6d6be2a2c7add5dcc23/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('Drive', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/f222fad9dd48d3d5bc85a2ac46548c3d/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('CPR', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/73d2b9be074bd3cd3f7f993bba0b8828/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('GrainsPulses', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/f062f32f964e2d7e4a2d01ccccb587db/bundle.js', [], () => ({
+  /* content */
+  /* handler:onCheckedStateAssignment */
+  async onCheckedStateAssignment(___arguments) {
+      return (!(await Backendless.Cache.contains('5')))
+
+  },  
+  /* handler:onCheckedStateAssignment */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/8ab3cfeb08046aaea72f441f317cf22b/bundle.js', [], () => ({
+  /* content */
+  /* handler:onCheckedStateAssignment */
+  async onCheckedStateAssignment(___arguments) {
+      return (!(await Backendless.Cache.contains('4')))
+
+  },  
+  /* handler:onCheckedStateAssignment */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/6fa26b560f85be8954c9083b214f527c/bundle.js', [], () => ({
+  /* content */
+  /* handler:onCheckedStateAssignment */
+  async onCheckedStateAssignment(___arguments) {
+      return (!(await Backendless.Cache.contains('3')))
+
+  },  
+  /* handler:onCheckedStateAssignment */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/85f1ddb635ea5487484b6d21549927c3/bundle.js', [], () => ({
+  /* content */
+  /* handler:onCheckedStateAssignment */
+  async onCheckedStateAssignment(___arguments) {
+      return (!(await Backendless.Cache.contains('7')))
+
+  },  
+  /* handler:onCheckedStateAssignment */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/9ddd6119130122d859639a0a677f3f8e/bundle.js', [], () => ({
+  /* content */
+  /* handler:onCheckedStateAssignment */
+  async onCheckedStateAssignment(___arguments) {
+      return (!(await Backendless.Cache.contains('')))
+
+  },  
+  /* handler:onCheckedStateAssignment */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/d9cf1676257aa0a4b28c0c6fd4b14564/bundle.js', [], () => ({
+  /* content */
+  /* handler:onCheckedStateAssignment */
+  async onCheckedStateAssignment(___arguments) {
+      return (!(await Backendless.Cache.contains('6')))
+
+  },  
+  /* handler:onCheckedStateAssignment */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/eb45e52f3814a9df2831bc6ad856f07b/bundle.js', [], () => ({
+  /* content */
+  /* handler:onCheckedStateAssignment */
+  async onCheckedStateAssignment(___arguments) {
+      return (!(await Backendless.Cache.contains('1')))
+
+  },  
+  /* handler:onCheckedStateAssignment */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/57fabe5a63c2b3cfd20e93941755f08c/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (){ window.history.back(); })();
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/AllTutorials2/components/47823eff70fba44dfca863cab7b8bdaf/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('Laundry', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/CPR/components/e7880bf2e2f211f6d52ddc0dde1a98f4/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      if ((await Backendless.Counters.get('len')) != 0) {
+    await Backendless.Cache.remove('6')
+    await Backendless.Counters.decrementAndGet('len');
+  }
+  ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/CPR/components/dbddd81a131608493a6b8f2b887a3fa5/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (url, isExternal, params){ const targetUrl = `${url}?${BackendlessUI.QueryString.stringify(params)}`; isExternal ? window.open(targetUrl, '_blank') : window.location = targetUrl; })('https://www.medicalnewstoday.com/articles/324712#cpr-step-by-step', true, null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/ChangeBulb/components/b62e13ff49a58125e50593597f064a47/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      if ((await Backendless.Counters.get('len')) != 0) {
+    await Backendless.Cache.remove('4')
+    await Backendless.Counters.decrementAndGet('len');
+  }
+  ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/ChangeBulb/components/bcdbfe3acbd12a8cbfccba7b48be13e8/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      await Backendless.Data.of('allTutorials').remove( 'C1EC93E7-16D8-4B1A-8535-6D5D15D32ABE' )
+  ;await ( async function (url, isExternal, params){ const targetUrl = `${url}?${BackendlessUI.QueryString.stringify(params)}`; isExternal ? window.open(targetUrl, '_blank') : window.location = targetUrl; })('https://www.wikihow.com/Change-a-Light-Bulb', true, null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/ChangeTyre/components/4f183e093473fbe80b5a0787ddc41216/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      if ((await Backendless.Counters.get('len')) == 0) {
+    await Backendless.Cache.remove('5')
+    await Backendless.Counters.decrementAndGet('len');
+  }
+  ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/Drive/components/7f229aa1655b5a216391cd17e0d79b8c/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (url, isExternal, params){ const targetUrl = `${url}?${BackendlessUI.QueryString.stringify(params)}`; isExternal ? window.open(targetUrl, '_blank') : window.location = targetUrl; })('https://www.wikihow.com/Drive-a-Car', true, null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/Drive/components/a44a3f4da32bf22f7ee8101e6797c0aa/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      if ((await Backendless.Counters.get('len')) != 0) {
+    await Backendless.Cache.remove('2')
+    await Backendless.Counters.decrementAndGet('len');
+  }
+  ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/GrainsPulses/components/e28ffb1e7cec81146d74d5bffedf63be/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (url, isExternal, params){ const targetUrl = `${url}?${BackendlessUI.QueryString.stringify(params)}`; isExternal ? window.open(targetUrl, '_blank') : window.location = targetUrl; })('https://www.asdagoodliving.co.uk/food/features/an-experts-guide-to-grains-and-pulses', true, null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/GrainsPulses/components/f68f677f7d098d5abd2eca296b683a8c/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      if ((await Backendless.Counters.get('len')) != 0) {
+    await Backendless.Cache.remove('1')
+    await Backendless.Counters.decrementAndGet('len');
+  }
+  ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/Laundry/components/31be07fe881498a8028d032158d893cd/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (url, isExternal, params){ const targetUrl = `${url}?${BackendlessUI.QueryString.stringify(params)}`; isExternal ? window.open(targetUrl, '_blank') : window.location = targetUrl; })('https://www.thespruce.com/how-to-do-laundry-2146149', true, null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/Laundry/components/ca5bba292fa50c1c8e5fd407f287d211/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      if ((await Backendless.Counters.get('len')) != 0) {
+    await Backendless.Cache.remove('3')
+    await Backendless.Counters.decrementAndGet('len');
+  }
+  ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/SelfDefense/components/482dce2f0b5c47e17f37efe98afcd68e/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      if ((await Backendless.Counters.get('len')) != 0) {
+    await Backendless.Cache.remove('7')
+    await Backendless.Counters.decrementAndGet('len');
+  }
+  ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('WelcomePage', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/SelfDefense/components/92ac437b9f66bda67e341cc85cd40974/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (url, isExternal, params){ const targetUrl = `${url}?${BackendlessUI.QueryString.stringify(params)}`; isExternal ? window.open(targetUrl, '_blank') : window.location = targetUrl; })('https://www.healthline.com/health/womens-health/self-defense-tips-escape', true, null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/WelcomePage/components/71384675b8c6b388cc4ed0898d963fb8/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })((await Backendless.Cache.get((JSON.stringify((await Backendless.Counters.get('len')))))), ___arguments.context.appData);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/WelcomePage/components/13d7bdcf55951fa41dc209ae42964c8b/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('AllTutorials2', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
+
+define('./pages/WelcomePage/components/7ccc0f3a533c5529b94a14dedc0b17d2/bundle.js', [], () => ({
+  /* content */
+  /* handler:onClick */
+  async onClick(___arguments) {
+      ;await ( async function (pageName, pageData){ BackendlessUI.goToPage(pageName, pageData) })('AboutUs', null);
+
+  },  
+  /* handler:onClick */
+  /* content */
+}))
